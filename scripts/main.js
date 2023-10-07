aff79db8320795a6f578a0d8765e6cac0bacb3b9
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2023-10-07 19:07:02 +0530

    added a scrolling indicatior

--- scripts/main.js
+++ scripts/main.js
@@ -36,7 +36,7 @@ var typed = new Typed('.typing', {
   });
 
   // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction() ,scrollFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -44,3 +44,23 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+ // Get the button:
+ let mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ 
+
+ function scrollFunction() {
+ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+ } else {
+     mybutton.style.display = "none";
+ }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+ document.body.scrollTop = 0; // For Safari
+ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+ }
