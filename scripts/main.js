6a551fa6f545ce1b5166076dd72c32f7e1cf734b
Sreekanth S <sreekanthsanthosh17@gmail.com>
2023-12-07 19:46:27 +0530

    ui updates

--- scripts/main.js
+++ scripts/main.js
@@ -89,9 +89,10 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
+        // else {
+        //     entry.target.classList.remove('show');
+        // }
     });
 });
  
