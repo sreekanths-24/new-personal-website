18290096a7b6ce54748915889f1d37171a2a85c1
Sreekanth S <sreekanthsanthosh17@gmail.com>
2023-12-09 22:07:59 +0530

    added my vision and mission to the about sectio

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,23 @@
 //smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
 
-        if (targetElement) {
-            // Use a cubic-bezier easing function for the animation
-            const cubicBezierTiming = 'cubic-bezier(0.42, 0, 0.58, 1)';
-            
-            targetElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-                inline: 'nearest',
-                timingFunction: cubicBezierTiming
-            });
-        }
-    });
+    if (targetElement) {
+      // Use a cubic-bezier easing function for the animation
+      const cubicBezierTiming = "cubic-bezier(0.42, 0, 0.58, 1)";
+
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+        timingFunction: cubicBezierTiming,
+      });
+    }
+  });
 });
-
 
 // var typed = new Typed('.logotype', {
 //     strings: [
@@ -35,72 +34,69 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //     showCursor: false,
 //     loop: true
 //   });
-var typed = new Typed('.typing', {
-    strings: [
-        'am Sreekanth S.', 
-        'am a Freelancing Web Developer.', 
-        'am a Full Stack Web Developer.', 
-        'am a Computer Science Engineer.',
-        'am a Tech Enthusiast.',
-        'love learning new technologies.',
-    ],
-    smartBackspace: true,
-    typeSpeed: 60,
-    backSpeed: 20,
-    backDelay: 2000,
-    loop: true,
-    showCursor: true,
-    cursorChar: "_"
-  });
+var typed = new Typed(".typing", {
+  strings: [
+    "am Sreekanth S.",
+    "am a Freelancing Web Developer.",
+    "am a Full Stack Web Developer.",
+    "am a Computer Science Engineer.",
+    "am a Tech Enthusiast.",
+    "love learning new technologies.",
+  ],
+  smartBackspace: true,
+  typeSpeed: 60,
+  backSpeed: 20,
+  backDelay: 2000,
+  loop: true,
+  showCursor: true,
+  cursorChar: "_",
+});
 
-  // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction() ,scrollFunction()};
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction(), scrollFunction();
+};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
- // Get the button:
- let mybutton = document.getElementById("myBtn");
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
- // When the user scrolls down 20px from the top of the document, show the button
- 
+// When the user scrolls down 20px from the top of the document, show the button
 
- function scrollFunction() {
- if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-     mybutton.style.display = "block";
- } else {
-     mybutton.style.display = "none";
- }
- }
-
- // When the user clicks on the button, scroll to the top of the document
- function topFunction() {
- document.body.scrollTop = 0; // For Safari
- document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
- 
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 // Intersection Observer API for scrolling animations
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        // else {
-        //     entry.target.classList.remove('show');
-        // }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+    // else {
+    //     entry.target.classList.remove('show');
+    // }
+  });
 });
- 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach(element => observer.observe(element));
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
-  
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => observer.observe(element));
+
